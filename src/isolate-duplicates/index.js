058289285b4a080eee1ad4ca6696696c17ebe021
Chibuike-ilo <chibuike.ilo@decagon.dev>
2022-09-12 12:35:56 +0100

    first commit of challenge 2

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,14 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+
+    if(typeof(text) === String) {
+        return "Please Enter a valid string";
+    }
+
+    text = text.split(" ");
+
+    for (char of MORSE_CODE) {
+        console.log(char);
+    }
+}
 
 module.exports = isolateDuplicates;
--- src/morse/index.js
+++ src/morse/index.js
@@ -58,6 +58,45 @@ const MORSE_CODE = {
 
 Object.freeze(MORSE_CODE);
 
-function morse(text) {}
+function morse(text) {
+  
+  // if (typeof(text) !== String) { 
+  //   return "Not a String"; //since morse function must not take a non string parameter
+  // }
 
+  let txt = text.trim() //morse code has no empty space character(s) hence we have to trim our input parameters
+
+  let morse_word = txt.split('   '); // since word code are seperated by 3 spaces, we first have to seperate all the words
+   
+
+  let morse_char = []; // an array for all morse char in our input string
+
+  let word = [];
+
+  for (let i = 0; i < morse_word.length; i++) {
+
+    morse_char.push(morse_word[i].split(' ')); // in morse, morse_char codes are seperated by single spaces hence, we have to create an array of single characters from every word code
+
+    // let morse_char = morse_word[i].split(' ');
+    // char_array.push(morse_char);
+
+
+  }
+  let char_array = [];
+
+  for (let i = 0; i < morse_char.length; i++) {
+
+    for (let j=0; j<morse_char[i].length; j++) {
+
+      char_array.push(((MORSE_CODE[morse_char[i][j]])));
+
+      if( j == char_array[i] -1) {
+        char_array.push(' ');
+      }
+
+    }
+  }
+  return char_array.toString().trim().replace(/,/g, '');
+
+}
 module.exports = morse;
